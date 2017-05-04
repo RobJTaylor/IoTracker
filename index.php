8da7d41239e812fb601e9e100878af6f7199dfc5
AFartingHippo <charliecross@hotmail.co.uk>
2017-05-04 18:42:48 +0100

    index page update

--- index.php
+++ index.php
@@ -14,7 +14,8 @@
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
-
+	<link href="css/custom.css" rel="stylesheet">
+	
     <!-- Custom styles for this template -->
     <link href="css/jumbotron.css" rel="stylesheet">
 
@@ -60,30 +61,34 @@
       </div>
     </div>
 
-    <div class="container">
+    <div class="container index-device">
       <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4">
-          <h2>Device 1</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
+	  <div class="index-device-left">
+		  <div class="row">
+			<div class="col-md-4">
+			  <h2>Device 1</h2>
+			  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+			  <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div>
+		  </div>
+		  <div class="row">
+			<div class="col-md-4">
+			  <h2>Device 2</h2>
+			  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+			  <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div>
+		  </div>
+		  <div class="row">
+			<div class="col-md-4">
+			  <h2>Device 3</h2>
+			  <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+			  <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+			</div>
+		  </div>
 	  </div>
-	  <div class="row">
-        <div class="col-md-4">
-          <h2>Device 2</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
+	  <div class="index-device-right">
+		<p>feowihfweio weiokfhw oh gha ogigh ohgh b qgihg ghiroghh gorihg hgiorhgh oehg  oghego  hgoi[hg  h</p>
 	  </div>
-	  <div class="row">
-        <div class="col-md-4">
-          <h2>Device 3</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-      </div>
-
       <hr>
 
       <footer>
